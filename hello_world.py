e806130eae318f9bf906b1c8dc961b310e54166e
Mark Rosales <mrosales@sxiq.com.au>
2020-03-03 13:43:45 +1100

    Changes from test2 branch

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,11 @@
-print('Hello World')
-print('This is a test program for Azure DevOps study')
-
+#print('Hello World')
+#print('This is a test program for Azure DevOps study')
 # changes from the branch test1
+#print('Test1 has been added on the branch')
+#print('Test has been mereged to master')
 
-print('Test1 has been added on the branch')
-print('Test has been mereged to master')
+# changes from test2
+import Tkinter
+top = Tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
